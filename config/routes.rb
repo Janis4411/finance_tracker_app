befb7d3f62211057ab26e964628777c9b0c6747b
Janis4411 <janis.vaneylen@gmail.com>
2022-04-20 12:36:56 +0200

    finished delete friends functionality, added create friend functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  devise_for :users
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
   get 'stocks/search'
   get 'users/my_portfolio'
   get 'search_friend', to: 'users#search'
   get 'my_friends', to: 'users#my_friends'
   resources :friendships, only: [:create, :destroy]
-  devise_for :users
-  root 'welcome#index'
-  get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stock', to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
